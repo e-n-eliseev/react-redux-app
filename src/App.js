1c6469351f4286401042b4b79dd825a998a01f4b
e-n-eliseev <e.n.eliseev@mail.ru>
2022-02-21 00:14:58 +0300

    base structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='left'>
+          <div className='img'>
+            <img className='photo' src='https://images.unsplash.com/photo-1612528443702-f6741f70a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80' alt='picture'></img>
+          </div>
+          <div className='description'></div>
+          <div className='description'></div>
+          <div className='btn_container'>
+            <button className='btn'>
+              &hearts;</button>
+            <button className='btn'>👎</button>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='comment'></div>
+          <ul className='comments_list' >
+
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
